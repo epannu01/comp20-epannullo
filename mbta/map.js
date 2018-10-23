022536ac2080c18dd259c561ac46d831bd98cc96
Emma Pannullo <emma.pannullo@tufts.edu>
2018-10-23 00:40:02 -0400

    worked out the path lines

--- mbta/map.js
+++ mbta/map.js
@@ -1,6 +1,8 @@
 var map_canvas;
 
-		var infowindow = new google.maps.InfoWindow();
+		var infowindow = new google.maps.InfoWindow({
+			maxWidth: 200
+		});
 
 		var map;
 	
@@ -15,34 +17,39 @@ var map_canvas;
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 
+		var pathCoords = [];
+		var pathCoords_size = 0;
+
+		var forkCoords = [];
+		var forkCoords_size = 0;
+
 		//polyline
 
 		var stops = [
-        	{position : new google.maps.LatLng(42.352271, -71.05524200000001), stop_id : "place-sstat", stop_name : "South Station"},
-        	{position : new google.maps.LatLng(42.330154, -71.057655), stop_id : "place-andrw", stop_name : "Andrew"},
-        	{position : new google.maps.LatLng(42.3884, -71.11914899999999), stop_id : "place-portr", stop_name : "Porter Square"},
-        	{position : new google.maps.LatLng(42.373362, -71.118956), stop_id : "place-harsq", stop_name : "Harvard Square"},
-        	{position : new google.maps.LatLng(42.320685, -71.052391), stop_id : "place-jfk", stop_name : "JFK/UMASS"},
-        	{position : new google.maps.LatLng(42.31129, -71.053331), stop_id : "place-shmnl", stop_name : "Savin Hill"},
-        	{position : new google.maps.LatLng(42.35639457, -71.0624242), stop_id : "place-pktrm", stop_name : "Park Street"},
+			{position : new google.maps.LatLng(42.395428, -71.142483), stop_id : "place-alfcl", stop_name : "Alewife"},
+			{position : new google.maps.LatLng(42.39674, -71.121815), stop_id : "place-davis", stop_name : "Davis"},
+			{position : new google.maps.LatLng(42.3884, -71.11914899999999), stop_id : "place-portr", stop_name : "Porter Square"},
+			{position : new google.maps.LatLng(42.373362, -71.118956), stop_id : "place-harsq", stop_name : "Harvard Square"},
+			{position : new google.maps.LatLng(42.365486, -71.103802), stop_id : "place-cntsq", stop_name : "Central Square"},
+			{position : new google.maps.LatLng(42.36249079, -71.08617653), stop_id : "place-knncl", stop_name : "Kendall/MIT"},
+			{position : new google.maps.LatLng(42.361166, -71.070628), stop_id : "place-chmnl", stop_name : "Charles/MGH"},
+			{position : new google.maps.LatLng(42.35639457, -71.0624242), stop_id : "place-pktrm", stop_name : "Park Street"},
+			{position : new google.maps.LatLng(42.355518, -71.060225), stop_id : "place-dwnxg", stop_name : "Downtown Crossing"},
+			{position : new google.maps.LatLng(42.352271, -71.05524200000001), stop_id : "place-sstat", stop_name : "South Station"},
         	{position : new google.maps.LatLng(42.342622, -71.056967), stop_id : "place-brdwy", stop_name : "Broadway"},
-        	{position : new google.maps.LatLng(42.275275, -71.029583), stop_id : "place-nqncy", stop_name : "North Quincy"},
+			{position : new google.maps.LatLng(42.330154, -71.057655), stop_id : "place-andrw", stop_name : "Andrew"},
+			{position : new google.maps.LatLng(42.320685, -71.052391), stop_id : "place-jfk", stop_name : "JFK/UMASS"},
+			{position : new google.maps.LatLng(42.31129, -71.053331), stop_id : "place-shmnl", stop_name : "Savin Hill"},
+        	{position : new google.maps.LatLng(42.300093, -71.061667), stop_id : "place-fldcr", stop_name : "Fields Corner"},
         	{position : new google.maps.LatLng(42.29312583, -71.06573796000001), stop_id : "place-smmnl", stop_name : "Shawmut"},
-        	{position : new google.maps.LatLng(42.39674, -71.121815), stop_id : "place-davis", stop_name : "Davis"},
-        	{position : new google.maps.LatLng(42.395428, -71.142483), stop_id : "place-alfcl", stop_name : "Alewife"},
-        	{position : new google.maps.LatLng(42.36249079, -71.08617653), stop_id : "place-knncl", stop_name : "Kendall/MIT"},
-        	{position : new google.maps.LatLng(42.361166, -71.070628), stop_id : "place-chmnl", stop_name : "Charles/MGH"},
-        	{position : new google.maps.LatLng(42.355518, -71.060225), stop_id : "place-dwnxg", stop_name : "Downtown Crossing"},
+        	{position : new google.maps.LatLng(42.284652,  -71.06448899999999), stop_id : "place-asmnl", stop_name : "Ashmont"},
+        	{position : new google.maps.LatLng(42.275275, -71.029583), stop_id : "place-nqncy", stop_name : "North Quincy"},
+        	{position : new google.maps.LatLng(42.2665139, -71.0203369), stop_id : "place-wlsta", stop_name : "Wollaston"},
         	{position : new google.maps.LatLng(42.251809, -71.005409), stop_id : "place-qnctr", stop_name : "Quincy Center"},
         	{position : new google.maps.LatLng(42.233391, -71.007153), stop_id : "place-qamnl", stop_name : "Quincy Adams"},
-        	{position : new google.maps.LatLng(42.284652,  -71.06448899999999), stop_id : "place-asmnl", stop_name : "Ashmont"},
-        	{position : new google.maps.LatLng(42.2665139, -71.0203369), stop_id : "place-wlsta", stop_name : "Wollaston"},
-        	{position : new google.maps.LatLng(42.300093, -71.061667), stop_id : "place-fldcr", stop_name : "Fields Corner"},
-        	{position : new google.maps.LatLng(42.365486, -71.103802), stop_id : "place-cntsq", stop_name : "Central Square"},
         	{position : new google.maps.LatLng(42.2078543, -71.0011385), stop_id : "place-brntn", stop_name : "Braintree"}
         ];
-        
-
+        			        
         // init function will make array of markers for each T-stop, sets the position of those
         // markers and calls functiton to add the schedule information
         function init()
@@ -65,7 +72,7 @@ var map_canvas;
 	          })(i, current_stop_id, current_marker, current_title));
 	          markers[i].marker.setMap(map);
 	    	}
-
+	    	makePath(markers);
 	    }
 
       	function getMyLocation() {
@@ -96,6 +103,7 @@ var map_canvas;
 					infowindow.setContent(marker.title);
 					infowindow.open(map, marker);
 				});
+
 			}
 
       	// use API and stop id of the T-station to get the schedule information
@@ -157,7 +165,7 @@ var map_canvas;
 
 				}
 				else if (request.readyState == 4 && request.status != 200) {
-					document.getElementById("location").innerHTML = "<p>Whoops, something went terribly wrongo</p>";
+					document.getElementById("location").innerHTML = "<p>Something went wrong</p>";
 				}
 				else {
 					console.log("In progress...");
@@ -167,8 +175,58 @@ var map_canvas;
       }
 
       function findTime (arrival_time) {
-      	var index = arrival_time.indexOf("T");
-      	var time_stamp = arrival_time.substr(index+1);
+      	begin = arrival_time.indexOf("T");
+      	end = arrival_time.lastIndexOf("-");
+      	time_stamp = arrival_time.substring(begin+1, end);
       	return time_stamp;
 
+      }
+
+      function makePath (markers) {
+        	for (i = 0; i < markers.length;i++) {
+        		path_lat = markers[i].marker.getPosition().lat();
+        		path_lng = markers[i].marker.getPosition().lng();
+
+        		if ((markers[i].stop_id != "place-nqncy") &&
+        			(markers[i].stop_id != "place-wlsta") &&
+        			(markers[i].stop_id != "place-qnctr") &&
+        			(markers[i].stop_id != "place-qamnl") &&
+        			(markers[i].stop_id != "place-brntn") &&
+        			(markers[i].stop_id != "place-jfk")) {
+
+        			pathCoords[pathCoords_size] = {lat: path_lat, lng: path_lng};
+        			pathCoords_size++;
+        		}
+        		else if (markers[i].stop_id == "place-jfk") {
+        			pathCoords[pathCoords_size] = {lat: path_lat, lng: path_lng};
+        			pathCoords_size++;
+        			forkCoords[forkCoords_size] = {lat: path_lat, lng: path_lng};
+        			forkCoords_size++;
+        		}
+        		else {
+
+        			forkCoords[forkCoords_size] = {lat: path_lat, lng: path_lng};
+        			forkCoords_size++;
+        		}
+        	}
+
+        	var path = new google.maps.Polyline({
+          	path: pathCoords,
+          	geodesic: true,
+          	strokeColor: '#FF0000',
+          	strokeOpacity: 1.0,
+          	strokeWeight: 2
+        	});
+
+        	path.setMap(map);
+
+        	var fork = new google.maps.Polyline({
+         	path: forkCoords,
+          	geodesic: true,
+          	strokeColor: '#FF0000',
+          	strokeOpacity: 1.0,
+          	strokeWeight: 2
+        	});
+
+        	fork.setMap(map);
       }
